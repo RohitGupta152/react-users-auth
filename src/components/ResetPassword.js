23233f75e90638301916a2a724bcc496a4e624d8
Rohit Gupta <107944384+RohitGupta152@users.noreply.github.com>
2025-01-31 06:48:30 -0800

    Update ResetPassword.js

--- src/components/ResetPassword.js
+++ src/components/ResetPassword.js
@@ -79,7 +79,7 @@ const ResetPassword = () => {
 
     setIsLoading(true);
     try {
-      const response = await fetch(`http://localhost:5000/api/auth/reset-password/${token}`, {
+      const response = await fetch(`https://new-auth-with-admin.vercel.app/api/auth/reset-password/${token}`, {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
