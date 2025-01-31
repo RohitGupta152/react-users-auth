7c5bd8a8a4fc006098af30be1eab367856d010c0
Rohit Gupta <107944384+RohitGupta152@users.noreply.github.com>
2025-01-31 06:47:49 -0800

    Update Register.js

--- src/components/Register.js
+++ src/components/Register.js
@@ -92,7 +92,7 @@ const Register = () => {
 
     setIsLoading(true);
     try {
-      const response = await fetch('http://localhost:5000/api/auth/register', {
+      const response = await fetch('https://new-auth-with-admin.vercel.app/api/auth/register', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
